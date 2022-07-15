9c4cf9594e1ce897337fdc01b32ce7f655f02e03
KK-Karan-Kumar <plexsido.kk@gmail.com>
2022-07-15 23:20:21 +0530

    enum in solidity

---
+++ 8.enums_is_solidity.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+
+// Enums are the way of creating user-defined data types, it is usually used to provide names for integral constants which makes the contract better for maintenance and reading. Enums restrict the variable with one of a few predefined values, these values of the enumerated list are called enums. Options are represented with integer values starting from zero, a default value can also be given for the enum. By using enums it is possible to reduce the bugs in the code.
+
+contract enumes{
+    enum size
+   {
+     SMALL,
+     MEDIUM,
+     LARGE
+   }
+
+   size public choice = size.MEDIUM;
+
+
+
+   //set size small
+   function setSmall() public{
+       choice = size.SMALL;
+   }
+
+
+   //set size medium
+    function setMedium() public{
+       choice = size.MEDIUM;
+   }
+
+
+   //set size large
+    function setLarge() public{
+       choice = size.LARGE;
+   }
+}
